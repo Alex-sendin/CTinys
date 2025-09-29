9eb7acd494cc3e0673420203577b83d446376685
Alex-sendin <alex.sendin@grupoecotisa.com>
2025-09-29 16:42:24 +0200

    add module fast landings

---
+++ modules/eco_fastlandings/controllers/front/eco_fastlandings.php
@@ -0,0 +1,45 @@
+<?php
+
+class eco_fastlandingseco_fastlandingsModuleFrontController extends ModuleFrontController
+{
+    public function initContent()
+    {
+        parent::initContent();
+
+        $pageName = Tools::getValue('page');
+
+        // Paso 1: Leer y decodificar el archivo JSON
+        $jsonFilePath = _PS_MODULE_DIR_ . $this->module->name . '/assets/json/data.json';
+        
+        if (file_exists($jsonFilePath)) {
+            $jsonContent = file_get_contents($jsonFilePath);
+            $landingPages = json_decode($jsonContent, true); // true para obtener un array asociativo
+        } else {
+            // Manejar el error si el archivo no existe (puedes redirigir a 404)
+            header('HTTP/1.1 404 Not Found');
+            header('Status: 404 Not Found');
+            $this->setTemplate('errors/not-found.tpl');
+            return;
+        }
+
+        // Paso 2: Buscar el case en los datos del JSON
+        $pageData = null;
+        foreach ($landingPages['landings'] as $page) {
+            if ($page['url'] === $pageName) {
+                $pageData = $page;
+                break;
+            }
+        }
+
+        if ($pageData) {
+            $this->context->smarty->assign('page_data', $pageData);
+            $this->setTemplate('module:eco_fastlandings/views/templates/front/'.$pageData['template']);
+        } 
+        else {
+            // Manejar el caso si el 'pageName' no se encuentra en el JSON
+            header('HTTP/1.1 404 Not Found');
+            header('Status: 404 Not Found');
+            $this->setTemplate('errors/not-found.tpl');
+        }
+    }
+}
---
+++ modules/eco_fastlandings/views/templates/front/layout_landings.tpl
@@ -0,0 +1,52 @@
+{extends file=$layout}
+
+{block name='head_seo'}
+  <title>{$page_data.meta_title}</title>
+  <meta name="description" content="{$page_data.meta_description}">
+{/block}
+
+{block name='content'}
+  <div id="tinys-landing-page" >
+
+    <div class="{$page_data.class_name} primary-banner">
+    </div>
+
+    <section class="tinys-landing-primary-content container">
+        {$page_data.primary_content nofilter}
+    </section>
+
+    <section class="tinys-landing-products-carousel container-fluid">
+        {hook h='displayTinysLandingProductsCarousel'}
+    </section>
+
+    <section class="tinys-landing-secondary-content container">
+        {$page_data.secondary_content nofilter}
+    </section>
+
+    <section class="tinys-landing-content-cards container-fluid">
+        {hook h='displayHome' mod='cards_home'}
+    </section>
+
+    <section class="tinys-landing-content-image">
+    </section>
+
+    <section class="tinys-landing-terciary-content container">
+        {$page_data.terciary_content nofilter}
+    </section>
+
+    {if isset($page_data.fourth_content)}
+      <section class="tinys-landing-fourth-content container">
+        {$page_data.fourth_content nofilter}
+      </section>
+    {/if}
+
+    {if isset($page_data.fifth_content)}
+      <section class="tinys-landing-fifth-content container">
+        {$page_data.fifth_content nofilter}
+      </section>
+    {/if}
+  </div>
+  <section class="tinys-landing-content-advisor container">
+    {hook h='displayTinysLandingAdvisor'}
+  </section>
+{/block}
